37233e548185cdfd40dd2bb741774284e9b17a2f
lalitchandra00 <mail4lalitchandra@gmail.com>
2026-08-09 01:56:01 +0530

    ui update

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,24 +1,47 @@
 import { useState } from 'react'
 
-
 function Login() {
     const [form, setForm] = useState('Signup')
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        alert('Form Submitted')
+    }
     return (
         <>
-            <div className = "flex min-h-screen items-center justify-center ">
-                
-                    <span className = "flex gap-2">
-                        <button className = "border-2 h-auto w-auto p-2" onClick={() = setForm('Signup')} >Signup Form</button>
-                        <button className = "border-2 h-auto w-auto p-2" onClick={() = setForm('Login')} >Login Form</button>
-                    </span>
-                    <form className = "`${form === 'Signup' ? 'ring-2' } border-2 rounded-xl h-auto w-auto p-2 flex flex-col gap-2 shadow-lg" onSubmit={handleSubmit}>
-                    <input className = "border-2 shadow-lg rounded-md p-1.5" label = 'Username' type="text" />
-                    <input className = "border-2 shadow-lg rounded-md p-1.5" label = 'Email' type="email" />
-                    <input className = "border-2 shadow-lg rounded-md p-1.5" label = 'Password' type="password" />
+            <div className="flex flex-col min-h-screen items-center justify-center gap-3">
+                <span className="flex gap-2">
+                    <button
+                        className={`p-2 border-2 rounded-2xl ${form === 'Signup' ? 'bg-blue-500 text-white' : ''}`}
+                        onClick={() => setForm('Signup')}
+                    >
+                        Signup Form
+                    </button>
+                    <button
+                        className={`p-2 border-2 rounded-2xl ${form === 'Login' ? 'bg-blue-500 text-white' : ''}`}
+                        onClick={() => setForm('Login')}
+                    >
+                        Login Form
+                    </button>
+                </span>
 
-                    <button type="submit">
+                <form className="border-2 rounded-xl w-72 p-5 flex flex-col gap-3 shadow-lg" onSubmit={handleSubmit}>
+                    {form === 'Signup'}
+
+                    <input className="border-2 shadow-sm rounded-md p-1.5" placeholder="Username" type="text" />
+                    <input className="border-2 shadow-sm rounded-md p-1.5" placeholder="Email" type="email" />
+                    <input className="border-2 shadow-sm rounded-md p-1.5" placeholder="Password" type="password" />
+                    <button className = "border-2 w-fit self-center p-1 rounded-md" type="submit">
                         Submit
-                    </button>   
+                    </button>
+                </form>
+
+                <form className="border-2 rounded-xl w-72 p-5 flex flex-col gap-3 shadow-lg" onSubmit={handleSubmit}>
+                    {form === 'Login'}
+                    <input className="border-2 shadow-sm rounded-md p-1.5" placeholder="Email" type="email" />
+                    <input className="border-2 shadow-sm rounded-md p-1.5" placeholder="Password" type="password" />
+                    <button className = "border-2 w-fit self-center p-1 rounded-md" type="submit">
+                        Submit
+                    </button>
                 </form>
             </div>
         </>
@@ -26,4 +49,3 @@ function Login() {
 }
 
 export default Login
-
