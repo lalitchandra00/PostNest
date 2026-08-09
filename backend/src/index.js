a2e5c1a58f41920fcb8a265853558baa22938fbd
lalitchandra00 <mail4lalitchandra@gmail.com>
2026-08-09 12:43:03 +0530

    backend-db

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,22 @@
 import mongoose from 'mongoose'
 import {PostNest_DB} from"./constants"
 
+import express from'express';
+const app = express();
+
+async function connectDB() {
+    try{
+        await mongoose.connect(process.env.MONGO_DB)
+        app.on("error", (error) =>{
+            throw error
+        
+        })
+        app.listen(process.env.PORT, () => { console.log('App is listening on port' + process.env.PORT)});
+    }
+    catch(error){
+        consle.log("error : " + error);
+    }
+    
+   
+}
+connectDB();
