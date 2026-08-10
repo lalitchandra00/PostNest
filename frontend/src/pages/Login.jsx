4d3e7588866c6660585158db41c80d545af7d451
lalitchandra00 <mail4lalitchandra@gmail.com>
2026-08-10 21:57:24 +0530

    login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import bgImage from "../assets/bgImage.png"
 import logo from "../assets/logo.svg"
+import group_users from "../assets/group_users.png"
 
 function Login() {
   return (
-    <div className = "min-h-screen">
-      <img className = "absolute top-0 left-0 h-full w-full"
-      src= {bgImage} alt="bgImage Error" />
-      <div>
-        <img className = "h-15"
-        src= {logo} alt="logo Error" />
+    <div className=" min-h-screen">
+
+      <img className="absolute top-0 left-0 h-full w-full"
+        src={bgImage} alt="bgImage Error" />
+
+      <div className = "flex flex-col">
+        <div>
+          <img className="h-15"
+            src={logo} alt="logo Error" />
+        </div>
+
+        <div className="flex flex-col">
+          <img className="h-6 w-15 flex items-center"
+            src={group_users} alt="group users"/>
+          Connect, share, and discover what matters to you.
+        </div>
       </div>
+
     </div>
   )
 }
