194882331e816866b91f3e21497c98408a785750
lalitchandra00 <mail4lalitchandra@gmail.com>
2026-08-11 14:13:25 +0530

    login  page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import {useState} from 'react'
 import bgImage from "../assets/bgImage.png"
 import logo from "../assets/logo.svg"
 import group_users from "../assets/group_users.png"
 
 function Login() {
+  const [form, setform] = useState('signup');
+
   return (
-    <div className=" min-h-screen">
+    <div className="min-h-screen flex flex-col">
 
       <img className="absolute top-0 left-0 h-full w-full"
         src={bgImage} alt="bgImage Error" />
+
 
       <div className = "flex flex-col">
         <div>
@@ -16,10 +20,29 @@ function Login() {
             src={logo} alt="logo Error" />
         </div>
 
-        <div className="flex flex-col">
-          <img className="h-6 w-15 flex items-center"
+        <div className="flex flex-col mt-60 ml-10">
+          <img className="h-10 w-25 flex items-baseline"
             src={group_users} alt="group users"/>
-          Connect, share, and discover what matters to you.
+            <span className = "font-bold text-3xl text-blue-700 " style={{ fontFamily: 'Roboto, sans-serif' }}>
+              Connect, share, and discover what matters to you.
+            </span>
+            <span className = "text-blue-800" style = {{fontFamily: "Playwrite IE, cursive"}}>
+              Find friends on PostNest
+            </span>
+
+            
+            {form === "login" ? 
+            <form className = "absolute right-5">
+                {form === "login"}
+                hii
+              </form>
+            :
+              <form className = "absolute right-25">
+                {form === "signup"}
+                <input className = "border" placeholder = "username" type="text" />
+              </form>
+            }
+              
         </div>
       </div>
 
