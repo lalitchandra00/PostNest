078f079fa6d750fd590c8acdb8b77379a98d7fc5
lalitchandra00 <mail4lalitchandra@gmail.com>
2026-08-15 23:29:12 +0530

    sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function sidebar() {
+function Sidebar({ Sidebar_open, setSidebar_open }) {
   return (
     <div>Sidebar</div>
   )
 }
 
-export default sidebar
+export default Sidebar
