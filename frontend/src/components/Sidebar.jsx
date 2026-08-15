8bf512624efde835ad07b708a62a9003a80aac66
lalitchandra00 <mail4lalitchandra@gmail.com>
2026-08-15 16:27:09 +0530

    layout and sidebar

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function sidebar() {
+  return (
+    <div>Sidebar</div>
+  )
+}
+
+export default sidebar
--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import {useState} from 'react'
+import { Heading3, Menu, X } from 'lucide-react'
+import Sidebar from '../components/Sidebar'
+import {dummyUserData} from "../assets/assets"
+
 
 function Layout() {
-  return (
-    <div>Layout</div>
+  const user = dummyUserData
+  const [Sidebar_open, setSidebar_open] = useState(true);
+  return  user ? (
+    <div className = "w-full flex w-screen">
+      <Sidebar />
+      {
+        Sidebar_open ?
+        <X className = "absolute top" onClick ={() => setSidebar_open(false)}/>
+        :
+        <Menu className = "" onClick ={() => setSidebar_open(true)}/>
+      }
+      <div>
+        Layout
+      </div>
+        
+
+    </div>
+  )
+  :
+  (
+    <h3>You are not logged in.</h3>
   )
 }
 
