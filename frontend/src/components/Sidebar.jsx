ad14cfc357c458b791014dc7519ec9c8eadf29c6
lalitchandra00 <mail4lalitchandra@gmail.com>
2026-08-16 22:53:10 +0530

    sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import logo from "../assets/logo.svg"
+import { useNavigate } from 'react-router-dom'
 
 function Sidebar({ Sidebar_open, setSidebar_open }) {
+  const navigate = useNavigate();
+
   return (
-    <div>Sidebar</div>
+    <div  className = "">
+      <aside className=" w-60 bg-gray-100 h-screen shadow-2xl">
+      <div>
+        <img className="-ml-0.5" src={logo} alt="Logo" onClick = {() => navigate('/')}/>
+        <hr className = "border-gray-300 mb-8" />
+      </div>
+      hii
+    </aside>
+    </div>
+    
+
   )
+
 }
 
 export default Sidebar
